8ebba814c18a2d1192ea159b6018b4d647320b74
diegoruelasgalaviz <diego.ruelas@live.com>
2021-06-29 15:06:30 -0500

    ariel

---
+++ index.js
@@ -0,0 +1,39 @@
+const cors = require("cors");
+const express = require("express");
+const stripe = require('stripe')(keys.stripeSecretKey);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Add your Stripe Secret Key to the .require('stripe') statement!");
+});
+
+app.post("/checkout", async (req, res) => {
+  console.log("Request:", req.body);
+
+  let error;
+  let status;
+  try {
+    
+    const charge = await stripe.charges.create({
+        amount: parseFloat(req.body.amount)*100,
+        currency: 'MXN',
+        description: 'Fidelio Shop',
+        source: req.body.token.id
+    });
+    res.send({msg: "success"}),
+      
+    console.log("Charge:", { charge });
+    status = "success";
+  } catch (error) {
+    console.error("Error:", error);
+    status = "failure";
+  }
+
+  res.json({ error, status });
+});
+
+app.listen(8080);
